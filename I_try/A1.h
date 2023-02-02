9ae1275371f89fe0fdae81a783792faa63cbeadc
Djon <z1henia@gmail.com>
2023-02-02 19:21:51 +0300

    1

---
+++ I_try/A1.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int A1(int &a, int &b);
---
+++ I_try/first_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <A1.h>
+
+int A1(int &a, int &b)
+{
+    std::cout<<"You can do"<<std::endl<<"1. sum"<<std::endl<<"2. multiplication";
+    std::cout<<"insert the number";
+    int number;
+    std::cin>>number;
+    int result =0;
+    if (number=1) {result=a+b;}
+    else if(number=2){ result=a*b;}
+    else {return 1;  }
+    
+    std::cout<<result;
+
+    return 0;
+}
